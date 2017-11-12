a71dcf1beba556c7a1808597ecf708ee90991bf3
Rehan Parkar <parkarcamp@gmail.com>
2017-11-12 16:44:56 -0600

    added UI elements

--- budget-shopper/controller/MainVC.swift
+++ budget-shopper/controller/MainVC.swift
@@ -10,10 +10,35 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    //outlets
+    @IBOutlet weak var wageTextField: hourlyWageTextField!
+    @IBOutlet weak var priceTextField: hourlyWageTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //porgramatically creating button over the number pad
+        customButton()
     }
+    
+    func customButton(){
+        
+        let calculateButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        
+        calculateButton.backgroundColor = #colorLiteral(red: 1, green: 0, blue: 0.07571224123, alpha: 1)
+        calculateButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calculateButton.setTitle("Calculate", for: .normal)
+        calculateButton.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        //to display the button when the textfield are clicked
+        //the view to display when TF becomes first repsonder
+        wageTextField.inputAccessoryView = calculateButton
+        priceTextField.inputAccessoryView = calculateButton
+        
+    }
+    
+    @objc func calculate(){}
 
 }
 
--- budget-shopper/view/hourlyWageTextField.swift
+++ budget-shopper/view/hourlyWageTextField.swift
@@ -10,18 +10,54 @@
 
 import UIKit
 
+//IBDesignable allows us to see the custom view in the story board
+
+@IBDesignable
+
 class hourlyWageTextField: UITextField {
     
+    
+    
+    
+    
+    //this runs the code needed to be seen live in the storyboard
+    override func prepareForInterfaceBuilder() {
+        customView()
+        
+    }
+    
+    //when textfield comes up from UI file then this is run
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        customView()
     }
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    func customView() {
+        
+        
+        //using color literal
+        //BG color white + opacity to 30
+        backgroundColor = #colorLiteral(red: 0.9999018312, green: 1, blue: 0.9998798966, alpha: 0.3)
+        //text color
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        //rounded corners of text field
+        layer.cornerRadius = 5
+        
+        // allignment of text to center of TF
+        textAlignment = .center
+        
+        //placeholder customization
+        
+        if let placeH = placeholder {
+            
+            let place = NSAttributedString(string: placeH, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            
+        }
+        
     }
-    */
 
 }
