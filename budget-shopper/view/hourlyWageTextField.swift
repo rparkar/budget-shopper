13d248cadd83becc81b015fdef56fd97b16a55ec
Rehan Parkar <parkarcamp@gmail.com>
2017-11-12 15:32:42 -0600

    first commit

---
+++ budget-shopper/view/hourlyWageTextField.swift
@@ -0,0 +1,27 @@
+//
+//  hourlyWageTextField.swift
+//  budget-shopper
+//
+//  Created by Rehan Parkar on 2017-11-12.
+//  Copyright © 2017 Rehan Parkar. All rights reserved.
+//
+//custom class to make the text field look the way we want it ot look
+
+
+import UIKit
+
+class hourlyWageTextField: UITextField {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
